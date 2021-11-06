82d820b4b45a3808cd8f646bd644c3c5c8b00c88
Deyaa M <deyaaabolata@gmail.com>
2021-11-07 01:24:41 +0200

    basic lang detection

--- lang_det.py
+++ lang_det.py
@@ -1,1 +1,9 @@
 # Language Detection
+from langdetect import detect
+
+with open('sample_2.json') as file:
+    while (line := file.readline().rstrip()):
+        lang = detect(line) 
+        print(lang)
+
+#lang = detect("en, fr, ger")
