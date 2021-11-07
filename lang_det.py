b5ad6cb48d2152555f5e5e428e32023dd3ba3270
Deyaa M <deyaaabolata@gmail.com>
2021-11-07 09:32:27 +0200

    adding sentiment analysis

--- lang_det.py
+++ lang_det.py
@@ -2,6 +2,8 @@
 from langdetect import detect
 from translate import Translator
 from deep_translator import GoogleTranslator
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # Read sample
 with open('sample_0.json') as file:
@@ -12,5 +14,15 @@ with open('sample_0.json') as file:
 
 # Translate
         translation = GoogleTranslator(source='auto', target='en').translate(line)
+
+# Sentiment Analysis
+        #tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+        #model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+        classifier = pipeline("sentiment-analysis")
+        #classifier('it is good to see you.')
+# output
         print(lang)
         print(translation)
+        print(classifier(line))
