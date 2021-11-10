59d6629d4a0f85ea30cfb5555b0907d6b2dd945f
Deyaa M <deyaaabolata@gmail.com>
2021-11-10 04:25:38 +0200

    read any json ext.

--- lang_det.py
+++ lang_det.py
@@ -5,27 +5,29 @@ from deep_translator import GoogleTranslator
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import json
+import glob
 
 
 # Read sample
-with open('sample_2.json') as file:
-    sample = json.load(file)
-    orig_review = sample['review_text'] #json.dumps(sample, indent=1)
-    #print(orig_review)
+for file in glob.glob('*.json'):
+    with open(file) as file:
+        sample = json.load(file)
+        orig_review = sample['review_text'] #json.dumps(sample, indent=1)
+        #print(orig_review)
 # Detect        
-    lang = detect(orig_review) 
+        lang = detect(orig_review) 
 
 # Translate
-    eng_review = GoogleTranslator(source='auto', target='en').translate(orig_review)
+        eng_review = GoogleTranslator(source='auto', target='en').translate(orig_review)
 
 # Sentiment Analysis
         #tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
         #model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
-    classifier = pipeline("sentiment-analysis")
+        classifier = pipeline("sentiment-analysis")
 
 # output
-    print(orig_review)
-    print(eng_review)
-    print(classifier(eng_review))
+        print(orig_review)
+        print(eng_review)
+        print(classifier(eng_review))
