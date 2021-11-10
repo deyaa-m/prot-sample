aab6dd224b5b3505aba19fccfbf059238d00278a
Deyaa M <deyaaabolata@gmail.com>
2021-11-11 00:29:45 +0200

    invoke lambda function when s3 upload

--- lang_det.py
+++ lang_det.py
@@ -9,25 +9,40 @@ import glob
 
 
 # Read sample
-for file in glob.glob('*.json'):
-    with open(file) as file:
-        sample = json.load(file)
-        orig_review = sample['review_text'] #json.dumps(sample, indent=1)
-        #print(orig_review)
-# Detect        
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        payload = response["Body"].read().decode()
+        sample = json.load(payload)
+        orig_review = sample['review_text']
+    #print(event)
+
+    #for file in glob.glob('*.json'):
+    #    with open(file) as file:
+    #        sample = json.load(file)
+    #        orig_review = sample['review_text'] #json.dumps(sample, indent=1)
+            #print(orig_review)
+    # Detect        
         lang = detect(orig_review) 
 
-# Translate
+    # Translate
         eng_review = GoogleTranslator(source='auto', target='en').translate(orig_review)
 
-# Sentiment Analysis
-        #tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+    # Sentiment Analysis
+            #tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
-        #model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+            #model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
         classifier = pipeline("sentiment-analysis")
 
-# output
+    # output
         print(orig_review)
         print(eng_review)
         print(classifier(eng_review))
+        return 'Success!'
+    except Exception as e:
+        print(e)
+        raise e
