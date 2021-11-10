0b49cb25f754ee5a3ae485fed0f805c746b7cbf6
Deyaa M <deyaaabolata@gmail.com>
2021-11-11 01:20:22 +0200

    libs imports

--- lang_det.py
+++ lang_det.py
@@ -5,15 +5,21 @@ from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import json
 import glob
+import urllib.parse
+import boto3
 
+print('Loading Function')
+
+s3 = boto3.client('s3')
 
 # Read sample
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
         payload = response["Body"].read().decode()
         sample = json.load(payload)
         orig_review = sample['review_text']
